b9b1c96031202c749d657da457b88e998fdfa2b3
SimonStHilaire <simon.sthilaire@gmail.com>
2019-10-29 20:57:41 -0400

    Initial commit

---
+++ Assets/Scripts/AverageManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class AverageManager : MonoBehaviour
+{
+    public RawImage DisplayImage;
+    public string ImageFilePath;
+    public string RequestFilePath;
+
+    public Slider MinAgeSlider;
+    public Slider MaxAgeSlider;
+
+    public Text MinAgeValueText;
+    public Text MaxAgeValueText;
+
+    public Toggle MaleToggle;
+    public Toggle FemaleToggle;
+
+    public Text ErrorText;
+
+    public GameObject LoadingPanel;
+    public float Timeout;
+
+    bool RequestPending = false;
+
+    [System.Serializable]
+    private struct RequestDefinition
+    {
+        public int minAge;
+        public int maxAge;
+        public string gender;
+    }
+
+    bool ImageLoading = false;
+    float Timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MinAgeSlider.onValueChanged.AddListener(OnNewMinAgeValue);
+        MaxAgeSlider.onValueChanged.AddListener(OnNewMaxAgeValue);
+    }
+
+    void OnNewMinAgeValue(float value)
+    {
+        MinAgeSlider.value = Mathf.Min(value, MaxAgeSlider.value);
+    }
+
+    void OnNewMaxAgeValue(float value)
+    {
+        MaxAgeSlider.value = Mathf.Max(value, MinAgeSlider.value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MinAgeValueText.text = MinAgeSlider.value.ToString();
+        MaxAgeValueText.text = MaxAgeSlider.value.ToString();
+        
+        if(RequestPending)
+        {
+            Timer -= Time.deltaTime;
+
+            if(Timer <= 0.0f)
+            {
+                ErrorText.text = "Aucun résultat";
+                StopCoroutine(LoadImage());
+                ImageLoading = false;
+                RequestPending = false;
+                LoadingPanel.SetActive(false);
+            }
+
+            if (!ImageLoading && File.Exists(ImageFilePath))
+            {
+                StartCoroutine(LoadImage());
+            }
+        }
+        
+    }
+
+    public void GenerateRequest()
+    {
+        ErrorText.text = "";
+        LoadingPanel.SetActive(true);
+
+        File.Delete(ImageFilePath);
+
+        RequestDefinition request = new RequestDefinition();
+        request.minAge = (int)MinAgeSlider.value;
+        request.maxAge = (int)MaxAgeSlider.value;
+        if(MaleToggle.isOn)
+            request.gender += "M";
+
+        if(FemaleToggle.isOn)
+            request.gender += "F";
+
+        File.WriteAllText(RequestFilePath, JsonUtility.ToJson(request, true));
+
+        RequestPending = true;
+        Timer = Timeout;
+    }
+
+    IEnumerator LoadImage()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        ImageLoading = true;
+
+        if (File.Exists(ImageFilePath))
+        {
+            UnityWebRequest www = UnityWebRequestTexture.GetTexture("file://" + System.Environment.CurrentDirectory + "/" + ImageFilePath);
+
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                DisplayImage.texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            }
+        }
+
+        ImageLoading = false;
+        RequestPending = false;
+        LoadingPanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/simplerotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class simplerotate : MonoBehaviour {
+
+    private RectTransform rectComponent;
+    private Image imageComp;
+    public float rotateSpeed = 200f;
+    private float currentvalue;
+    public Vector3 RotateAxis;
+
+    void Start()
+    {
+        rectComponent = GetComponent<RectTransform>();
+        imageComp = rectComponent.GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        rectComponent.transform.Rotate(RotateAxis * rotateSpeed * Time.deltaTime);
+    }
+}
